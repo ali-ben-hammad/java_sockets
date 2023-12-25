76633bdc83a850a1e5a70546681e7911db02a9ac
malcomer123 <achrafhammi29@gmail.com>
2023-12-25 12:28:23 +0100

    latest

--- src/main/java/org/project/ChatThread.java
+++ src/main/java/org/project/ChatThread.java
@@ -2,7 +2,6 @@ package org.project;
 
 import java.io.*;
 import java.net.Socket;
-import java.util.List;
 import java.util.Map;
 
 public class ChatThread implements Runnable{
@@ -14,25 +13,26 @@ public class ChatThread implements Runnable{
         this.users = users;
     }
 
-    private void sendMessage(String message, String dest) throws IOException {
+    private void sendMessage(String src, String message, String dest) throws IOException {
         Socket destSocket = users.get(dest);
+        if(destSocket == null) return;
         OutputStream os = destSocket.getOutputStream();
         PrintWriter pw = new PrintWriter(os, true);
-        pw.println(message);
+        pw.println(src+ ": "+ message);
     }
 
     @Override
     public void run() {
         try {
-            String userName = "user" + (users.size()+1);
+            String userName = "user" + (users.size());
             users.put(userName, socket);
             InputStream is = socket.getInputStream();
             BufferedReader br = new BufferedReader(new InputStreamReader(is));
-            while(socket.isConnected()) {
+            while(true) {
                 String receivedMsg = br.readLine();
                 String[] split = receivedMsg.split("=>");
                 System.out.println(receivedMsg);
-                //sendMessage(split[0], split[1]);
+                sendMessage(userName, split[0], split[1]);
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
--- src/main/java/org/project/Main.java
+++ src/main/java/org/project/Main.java
@@ -1,19 +1,23 @@
 package org.project;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        try (ServerSocket serverSocket = new ServerSocket(8080)){
+            Map<String, Socket> clients = new HashMap<>();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+            while(true){
+                Socket s = serverSocket.accept();
+                new Thread(new ChatThread(s, clients)).start();
+            }
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+8.
-            System.out.println("i = " + i);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
